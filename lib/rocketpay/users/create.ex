5b10e2b588e659e4f29e416c8a4ceb143f09007b
Cézar Augusto <cezaraf@gmail.com>
2021-02-24 16:58:57 -0300

    Continuação
    * criando função de depósito

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,8 +1,25 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{Repo, User}
+  alias Ecto.Multi
+  alias Rocketpay.{Account, Repo, User}
 
   def call(params) do
-    params |> User.changeset() |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, &insert_account/2)
+    |> Multi.run(:preload_data, &preload_data/2)
+    |> run_transaction()
   end
 
+  defp insert_account(repo, %{create_user: user}), do: user.id |> account_changeset() |> repo.insert()
+
+  defp preload_data(repo, %{create_user: user}), do: {:ok, repo.preload(user, :account)}
+
+  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
+  end
 end
